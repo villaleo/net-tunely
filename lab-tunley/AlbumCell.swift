1f8f5ac1b36a392e5cf75dc95eedf4ecc023e723
Leo Villalobos <villaleobos@gmail.com>
2023-02-22 19:50:41 -0800

    Add collection view with AlbumCell

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,12 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Leonardo Villalobos on 2/22/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    @IBOutlet weak var albumImageView: UIImageView!
+}
